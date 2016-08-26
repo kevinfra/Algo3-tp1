9110170cec3e0b9811d618b0e6eacc241f07ef99
julianlen <julianlen@gmail.com>
2016-08-26 18:05:57 -0300

    Ordene todo y arranca el informe

---
+++ src/ejercicio3.cpp
@@ -0,0 +1,29 @@
+void Mochilero()
+{
+	vector<int> pesos;
+	vector<int> valores;
+	vector<int> mochilas;
+
+	switch (mochilas.size())
+	{
+		case 1:
+		break;
+
+		case 2:
+		break;
+
+		case 3:
+		/*
+		pesos x, y, z
+		*/
+		int peso1 = vector[0];
+		int peso2 = vector[1];
+		int peso3 = vector[2];
+
+		int matriz[peso1][peso2][peso3];
+
+		
+
+		break;
+	}
+}
