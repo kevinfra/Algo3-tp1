ebd65219d050d23878bd69a847527ea0d0c0de3d
julianlen <julianlen@gmail.com>
2016-09-02 21:06:59 -0300

    version inicial del ej3

--- src/ejercicio3.cpp
+++ src/ejercicio3.cpp
@@ -4,6 +4,44 @@ void Mochilero()
 	vector<int> valores;
 	vector<int> mochilas;
 
+	//objetos<mochila1<mochila2<valor>>>
+	vector<vector<vector<int>>>> cuboMagico;
+
+	vector<vector<int>> objetoNulo;
+
+	objetoNulo.resize(mochilas[0]);
+	for (int iMochila1 = 0; i < mochilas[0]; ++iMochila1)
+	{
+		objetoNulo[iMochila1].resize(mochilas[1]);
+		for (int iMochila2 = 0; i < mochilas[1]; ++iMochila2)
+		{
+			objetoNulo[iMochila1][iMochila2] = 0;
+		}
+	}
+	cuboMagico.push_back(objetoNulo);
+
+	for (int iTesoro = 0; iTesoro < pesos.size(); ++iTesoro)
+	{
+		for (int iMochila1 = 0; iMochila1 <= mochilas[0]; ++iMochila1)
+		{
+			for (int iMochila2 = 0; iMochila2 <= mochilas[1]; ++iMochila2)
+			{
+				if (iMochila1 == 0) {
+					if (valor[iTesoro])
+				}
+
+
+				 && iMochila2 == 0) {
+					cuboMagico[iTesoro][iMochila1][iMochila2] = 0;
+				}
+				else {
+
+				}
+
+			}
+		}
+	}
+
 	switch (mochilas.size())
 	{
 		case 1:
